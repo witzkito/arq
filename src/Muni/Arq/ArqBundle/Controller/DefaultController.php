bb5719101e90bbca9ee9c5468bb14e4834e5eeca
Leanwit <leanwitzke@gmail.com>
2013-10-09 11:07:00 -0300

    Aprendiendo
    Agregado una ruta de crearCalle donde al acceder crea una tupla definido
    por defecto. No puedo hacer que cuando ingreso a crear calle me tome el
    index.twig

--- src/Muni/Arq/ArqBundle/Controller/DefaultController.php
+++ src/Muni/Arq/ArqBundle/Controller/DefaultController.php
@@ -2,12 +2,36 @@
 
 namespace Muni\Arq\ArqBundle\Controller;
 
+use Muni\Arq\ArqBundle\Entity\Calle;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('ArqBundle:Default:index.html.twig', array('name' => $name));
+    
+    
+    public function principalAction(){
+       return $this->render('ArqBundle:Default:index.html.twig');
     }
+    
+  
+ 
+public function createAction()
+{
+    
+
+    $calle = new Calle();
+    $calle->setNombre('San Martin');
+    $calle->setNumeroinicio('200');
+    $calle->setNumeroFin('300');
+ 
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($calle);
+    $em->flush(); 
+   
+    return new Response('Created product id '.$calle->getId());
+
+    
+}
+
 }
